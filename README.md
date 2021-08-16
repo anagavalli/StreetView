15b19c27e35628f5c8747852543c29de88017030
Arnesh Nagavalli <arnesh.nagavalli46@gmail.com>
2021-08-16 14:51:49 -0700

    Allow scene transitions

--- README.md
+++ README.md
@@ -3,9 +3,4 @@ WebGL based clone of Google Street View, with limited functionality. Images are 
 
 Run "python -m SimpleHTTPServer" from the home directory then navigate to localhost:[portnum] to view the app.
 
-Currently only supports viewing a single default scene.
-
-Incoming features:
-- Navigate to additional scenes
-
-Note that adding user provided scenes is NOT a planned feature.
+Note that adding user provided scenes is neither implemented nor planned.
--- js/webgl-util.js
+++ js/webgl-util.js
@@ -5,6 +5,19 @@ const mod = (a, n) => ((a % n ) + n ) % n;
 
 const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
+function distance(p1, p2) {
+  const deltaX = p2.x - p1.x;
+  const deltaY = p2.y - p1.y;
+
+  return Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+}
+
+const degToRads = (deg) => deg * Math.PI / 180;
+
+function boundsContain(point, min, max) {
+  return min.x < point.x && point.x < max.x && min.y < point.y && point.y < max.y;
+}
+
 function initializeWebGL(canvas) {
   // Getting WebGL context the right way
   let gl = null;
@@ -177,4 +190,127 @@ function getImageMetadata(queue, callback) {
     promises.push(getGPS(queueItem.rawResult, queueItem.item.id));
   }
   Promise.all(promises).then(callback);
+}
+
+// Convenience method to convert from degrees/minutes/seconds to decimal values.
+const degToDecimal = arr => arr[0] + arr[1] / 60 + arr[2] / 3600;
+
+function findBearing(p1, p2) {
+  const deltaL = degToRads(p2.x - p1.x);
+  const x = Math.cos(degToRads(p2.y)) * Math.sin(deltaL);
+  const y = Math.cos(degToRads(p1.y)) * Math.sin(degToRads(p2.y)) - Math.sin(degToRads(p1.y)) * Math.cos(degToRads(p2.y)) * Math.cos(deltaL);
+
+  let bearing = Math.atan2(y, x);
+  if (bearing < 0) { bearing += Math.PI; }
+  return bearing * 180 / Math.PI;
+}
+
+// MST implementation taken from https://girlsincode.com/javascript/minimum-spanning-tree-in-an-undirected-weighted-graph-kruskal/
+class Edge {
+  constructor(v1, v2, w = 0, bearing = 0) {
+    this.v1 = v1;
+    this.v2 = v2;
+    this.w = w;
+    this.bearing = bearing;
+  }
+}
+
+class Graph {
+  constructor(v, e) {
+    this.v = v;
+    this.e = e;
+    this.edges = [];
+    this.nodes = [];
+  }
+
+  addEdge(edge) {
+    this.edges.push(edge);
+    if (!this.nodes.includes(edge.v1)) {
+      this.nodes.push(edge.v1);
+    }
+    if (!this.nodes.includes(edge.v2)) {
+      this.nodes.push(edge.v2);
+    }
+  }
+
+  getEdge(pos) {
+    return this.edges[pos]
+  }
+
+  getEdges() {
+    return this.edges
+  }
+
+  getNodes() {
+    return this.nodes
+  }
+
+  // get the root of node
+  find(subsets, node) {
+    let nodeInfo = subsets.get(node);
+    if (nodeInfo.parent !== node) {
+      nodeInfo.parent = this.find(subsets, nodeInfo.parent)
+    }
+
+    return nodeInfo.parent;
+  }
+
+  // unite the x and y subsets based on rank
+  union(subsets, x, y) {
+    let xroot = this.find(subsets, x);
+    let yroot = this.find(subsets, y);
+
+    if (subsets.get(xroot).rank < subsets.get(yroot).rank) {
+      subsets.get(xroot).parent = yroot;
+    } else if (subsets.get(xroot).rank > subsets.get(yroot).rank) {
+      subsets.get(yroot).parent = xroot;
+    } else {
+      subsets.get(yroot).parent = xroot;
+      subsets.get(xroot).rank++;
+    }
+  }
+}
+
+function kruskal(gNodes, gEdges, gFrom, gTo, gWeight, gBearing) {
+  let i = 0, j = 0, cost = 0;
+  let subsets = new Map(),
+    result = [];
+
+  let graph = new Graph(gNodes, gEdges);
+
+  while(i < gEdges) {
+    graph.addEdge(new Edge(gFrom[i], gTo[i], gWeight[i], gBearing[i]));
+    i++;
+  }
+
+  graph.getEdges().sort((edge1, edge2) => {
+    if (edge1.w === edge2.w) {
+      return 1;
+    }
+
+    return edge1.w < edge2.w ? -1 : 1;
+  });
+
+  console.log('sorted edges:' , graph.getEdges());
+
+  graph.getNodes().forEach(node => {
+    subsets.set(node, { parent: node, rank: 0 });
+  });
+
+  i = 0;
+  while(j < gNodes-1) {
+    let edge = graph.getEdge(i++);
+    let root1 = graph.find(subsets, edge.v1);
+    let root2 = graph.find(subsets, edge.v2);
+
+    // if the nodes doesn't create a cycle then we add the edge to final subgraph
+    if (root1 !== root2) {
+      result[j++] = edge;
+      // update the total weight of the subgraph
+      cost += edge.w;
+      graph.union(subsets, root1, root2);
+    }
+  }
+
+  return result;
 }
